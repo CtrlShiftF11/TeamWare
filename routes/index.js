89eff7c8c142822c922887cee9a6a1fdc710344b
Andy Sandefer <andy.sandefer@nextgearcapital.com>
2015-02-17 22:45:31 -0500

    Cosmetic EJS template changes for page title values and markup structure

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'TeamWare' });
+    res.render('index', { title: 'TeamWare | Home' });
 });
 
 router.get('/views/projects', function (req, res) {
-    res.render('projects', {title: 'TeamWare' });
+    res.render('projects', {title: 'TeamWare | Projects' });
 });
 
 router.get('/views/teams', function (req, res) {
-    res.render('teams', { title: 'TeamWare' });
+    res.render('teams', { title: 'TeamWare | Teams' });
 });
 
 router.get('/views/sprints', function (req, res) {
-    res.render('sprints', { title: 'TeamWare' });
+    res.render('sprints', { title: 'TeamWare | Sprints' });
 });
 
 router.get('/views/sprintsbyteam', function (req, res) {
