49eab5e619496e6bba90e8bd81ce811bdbb67811
Andy Sandefer <asandefer@gmail.com>
2015-03-01 12:04:28 -0500

    Too big to fail! -Massive refactor to Teams page.  Soon all CRUD pages will be redesigned to work like this (see comments/picture). -Refactored chart to experiment with slicing the chart into JavaScript fragments so that I could render the same chart in multiple places and various sizes (it worked!) -Switched from inline grid editing to dialog based editing

---
+++ public/javascripts/charts/actualvsplannedbyteam.js
@@ -0,0 +1,42 @@
+//Andy Sandefer
+function getSprintByTeam(teamId) {
+    $.getJSON('/sprints/getbyteam/' + teamId)
+        .done(function (sprintData) {
+            renderChart(sprintData);
+        });
+}
+
+function renderChart(sprintData) {
+    var chartOptions = {
+        chart: {
+            renderTo: 'teamChart',
+            type: 'spline'
+        },
+        title: {
+            text: 'Planned vs. Actual by Sprint'
+        },
+        xAxis: {
+            type: 'datetime',
+            labels: {
+                formatter: function () {
+                    return Highcharts.dateFormat('%m/%d/%y', this.value);
+                }
+            }
+        },
+        series: [
+            {
+                name: 'Actual Points',
+                data: []
+            },
+            {
+                name: 'Planned Points',
+                data: []
+            }
+        ]
+    };
+    for (var i = 0; i < sprintData.length; i++) {
+        chartOptions.series[0].data.push([Date.parse(sprintData[i].end_date), sprintData[i].actual_points]);
+        chartOptions.series[1].data.push([Date.parse(sprintData[i].end_date), sprintData[i].planned_points]);
+    }
+    var chart = new Highcharts.Chart(chartOptions);
+}
---
+++ public/javascripts/utils.js
@@ -0,0 +1,12 @@
+function getUrlParam(name, url) {
+    console.log(name);
+    console.log(url);
+    if (!url) {
+        url = window.location.href;
+    }
+    var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(url);
+    if (!results) {
+        return undefined;
+    }
+    return results[1] || undefined;
+}
