25ea1bb7ed5eb9326f6cebd97f26ee792626f235
Andy Sandefer <asandefer@gmail.com>
2015-03-05 23:33:44 -0500

    Fixed horrific date issue that I've been struggling to understand.  It was a multifaceted problem - Kendo UI is doing wonky display stuff which I've suppressed but probably not entirely solved and the actual data for dates that was coming back from MongoDB needed to be washed through a function (added to utils.js) in order to be usable by date picker widgets.

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -10,3 +10,16 @@ function getUrlParam(name, url) {
     }
     return results[1] || undefined;
 }
+
+function reformatDateForInput(dateVal) {
+    var yearVal = dateVal.getFullYear().toString();
+    var monthVal = (dateVal.getMonth() + 1).toString();
+    if (monthVal.length == 1) {
+        monthVal = '0' + monthVal;
+    }
+    var dateVal = (dateVal.getDate()).toString();
+    if (dateVal.length == 1) {
+        dateVal = '0' + dateVal;
+    }
+    return monthVal + '/' + dateVal + '/' + yearVal;
+}
